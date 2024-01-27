cda13b1baa78bac57be056ae5724d04ea24aa465
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2024-01-28 01:56:38 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 "use strict";
 
-$(function () {
+$(document).ready(function () {
   $("#header").load("../sections/header/header.html");
-});
-
-$(function () {
   $("#hero").load("../sections/hero/hero.html");
-});
-
-$(function () {
   $("#section-one").load("../sections/section-one/section-one.html");
+  $("#section-two").load("../sections/section-two/section-two.html");
+  $("#section-three").load("../sections/section-three/section-three.html");
+  $("#section-four").load("../sections/section-four/section-four.html");
+  $("#section-five").load("../sections/section-five/section-five.html");
+  $("#section-six").load("../sections/section-six/section-six.html");
+  $("#section-seven").load("../sections/section-seven/section-seven.html");
+  $("#footer").load("../sections/footer/footer.html");
 });
